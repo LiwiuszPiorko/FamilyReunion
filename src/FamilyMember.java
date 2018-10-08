77a194daa4bd7cfdf21745a610d2915747e12eff
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-10-08 10:06:45 +0200

    v2

---
+++ src/FamilyMember.java
@@ -0,0 +1,50 @@
+import java.util.Objects;
+
+public class FamilyMember {
+    private String name;
+    private int age;
+
+    public FamilyMember(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FamilyMember that = (FamilyMember) o;
+        return age == that.age &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "FamilyMember{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+}
--- src/FamilyReunion.java
+++ src/FamilyReunion.java
@@ -1,22 +1,29 @@
-import java.util.Arrays;
 import java.util.Scanner;
-
+import java.util.Arrays;
 
 public class FamilyReunion {
     public static void main(String[] args) {
 
-        String[] familyReunion = new String[3];
-        Scanner imiona = new Scanner(System.in);
-        System.out.println("Wpisz imię członków rodziny i ich wiek: ");
-        for (int i = 0; i < familyReunion.length; i++)
-            familyReunion[i] = imiona.nextLine();
-        {
-            if (imiona.nextLine().equals(imiona)) {
-                System.out.println("Tego członka rodziny już wpisałeś. Podaj imie i wiek jeszcze raz: ");
-            } else {
-                String arrayFamilyReunion = Arrays.toString(familyReunion);
-                System.out.println(arrayFamilyReunion);
-            }
-        }
+        FamilyMember[] familyReunion = new FamilyMember[3];
+        Scanner family = new Scanner(System.in);
+        System.out.println("Podaj imię członka rodziny");
+        String name = family.nextLine();
+        System.out.println("Podaj wiek członka rodziny");
+        int age = family.nextInt();
+        FamilyMember familyMember = new FamilyMember(name, age);
+
+        for (int i=0; i < familyReunion.length; i++) {//co dalej???
+            familyReunion[i]=family.
+        String reunion = Arrays.toString(familyReunion);
+        System.out.println(reunion);
+
+
     }
+
 }
+
+
+
+
+
+
